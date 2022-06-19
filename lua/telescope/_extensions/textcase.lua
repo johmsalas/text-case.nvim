0b4ba3489504c87dfa9d1266d60af47890e3a7a3
Johny Salas <johmsalas@gmail.com>
2022-06-18 21:30:30 -0500

    Separate normal and visual mode in Telescope

--- lua/telescope/_extensions/textcase.lua
+++ lua/telescope/_extensions/textcase.lua
@@ -11,15 +11,17 @@ local function invoke_replacement(prompt_bufnr)
     local change = selection.value
     if type(change) ~= 'table' then return end
 
-    if change.type == constants.change_type.QUICK_REPLACE then
-      plugin.quick_replace(change.method_desc)
+    if change.type == constants.change_type.CURRENT_WORD then
+      plugin.current_word(change.method_desc)
     elseif change.type == constants.change_type.LSP_RENAME then
       plugin.lsp_rename(change.method_desc)
+    elseif change.type == constants.change_type.VISUAL then
+      plugin.visual(change.method_desc)
     end
   end
 end
 
-local function telescope_textcase(opts)
+local function telescope_normal_mode_change(opts)
   opts = opts or require("telescope.themes").get_cursor()
   local results = {}
 
@@ -36,10 +38,10 @@ local function telescope_textcase(opts)
     api.to_title_case,
     api.to_path_case,
   }) do
-    local quick_replace_table = {
+    local current_word_table = {
       display = 'Convert to ' .. method.desc,
       method_desc = method.desc,
-      type = constants.change_type.QUICK_REPLACE,
+      type = constants.change_type.CURRENT_WORD,
     }
 
     local lsp_replace_table = {
@@ -48,8 +50,56 @@ local function telescope_textcase(opts)
       type = constants.change_type.LSP_RENAME,
     }
 
-    table.insert(results, quick_replace_table)
+    table.insert(results, current_word_table)
     table.insert(results, lsp_replace_table)
+  end
+
+  require("telescope.pickers").new(opts, {
+    prompt_title = "Text Case",
+    finder = require("telescope.finders").new_table({
+      results = results,
+      entry_maker = function(entry)
+        return {
+          value = entry,
+          display = entry.display,
+          ordinal = entry.display,
+        }
+      end,
+    }),
+    sorter = require("telescope.config").values.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      local curried_method = invoke_replacement(prompt_bufnr)
+      map("i", "<CR>", curried_method)
+      map("n", "<CR>", curried_method)
+      return true
+    end,
+  }):find()
+end
+
+local function telescope_visual_mode_change(opts)
+  opts = opts or require("telescope.themes").get_cursor()
+  local results = {}
+
+  for _, method in pairs({
+    api.to_upper_case,
+    api.to_lower_case,
+    api.to_snake_case,
+    api.to_dash_case,
+    api.to_constant_case,
+    api.to_dot_case,
+    api.to_phrase_case,
+    api.to_camel_case,
+    api.to_pascal_case,
+    api.to_title_case,
+    api.to_path_case,
+  }) do
+    local visual_replace_table = {
+      display = 'Convert to ' .. method.desc,
+      method_desc = method.desc,
+      type = constants.change_type.VISUAL,
+    }
+
+    table.insert(results, visual_replace_table)
   end
 
   require("telescope.pickers").new(opts, {
@@ -76,6 +126,7 @@ end
 
 return require("telescope").register_extension({
   exports = {
-    change = telescope_textcase,
+    normal_mode = telescope_normal_mode_change,
+    visual_mode = telescope_visual_mode_change,
   },
 })
--- lua/textcase/shared/constants.lua
+++ lua/textcase/shared/constants.lua
@@ -7,6 +7,7 @@ M.change_type = {
   LSP_RENAME = 'LSP_RENAME',
   CURRENT_WORD = 'CURRENT_WORD',
   QUICK_REPLACE = 'QUICK_REPLACE',
+  VISUAL = 'VISUAL'
 }
 
 return M
