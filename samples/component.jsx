fadb5ea495a7f20a088b84b67cd46b2b90c1bc36
Johny Salas <johmsalas@gmail.com>
2022-09-08 09:54:27 -0500

    bugfix: Fixed issue on LSP rename

--- samples/component.jsx
+++ samples/component.jsx
@@ -1,6 +1,7 @@
 import StepOne from './components/step-one';
 
-const SampleWizard = () => {
+const SampleWizard = ({ var1, var2 }) => {
+  console.log({ var2 })
   const [currentStep, setCurrentStep] = useState(1)
   if (currentStep === steps.STEP_ONE) {
     return <StepOne />
