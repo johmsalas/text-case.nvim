28e93c2b967da946ecd7e395cf0788f5767aa7fc
johny salas <johmsalas@gmail.com>
2023-11-19 10:30:16 -0500

    Added implementation details for minimal tests and sample for other envs

--- tests/environments/minimal.lua
+++ tests/environments/minimal.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 function M.root(root)
-  local f = debug.getinfo(1, "S").source:sub(3)
-  return vim.fn.fnamemodify(f, ":p:h:h") .. "/" .. (root or "")
+  local f = debug.getinfo(1, "S").source:sub(2)
+  return vim.fn.fnamemodify(f, ":p:h:h:h") .. "/" .. (root or "")
 end
 
+-- TODO: Call setup method from the utils file
 function M.setup(root_env)
   vim.cmd([[set runtimepath=$VIMRUNTIME]])
   vim.opt.runtimepath:append(M.root())
@@ -12,8 +13,7 @@ function M.setup(root_env)
 end
 
 function M.init()
-  -- TODO: Call setup method from the utils file
-  M.setup(".tests/telescope")
+  M.setup(".tests/minimal")
 end
 
 M.init()
--- tests/environments/test-env-helpers.lua
+++ tests/environments/test-env-helpers.lua
@@ -1,5 +1,10 @@
 local M = {}
 
+function M.root(root)
+  local f = debug.getinfo(1, "S").source:sub(2)
+  return vim.fn.fnamemodify(f, ":p:h:h:h") .. "/" .. (root or "")
+end
+
 function M.setup(root_env)
   vim.cmd([[set runtimepath=$VIMRUNTIME]])
   vim.opt.runtimepath:append(M.root())
