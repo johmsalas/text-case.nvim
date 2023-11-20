afe37b16dd0e0760d4e56a64b1a32b4d9897a54d
Peter Morgenstern <mail@petergundel.de>
2023-11-20 08:40:04 +0100

    Extract common test helpers

---
+++ tests/test_helpers.lua
@@ -0,0 +1,13 @@
+M = {}
+
+M.get_buf_lines = function()
+  local result = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+  return result
+end
+
+M.execute_keys = function(feedkeys)
+  local keys = vim.api.nvim_replace_termcodes(feedkeys, true, false, true)
+  vim.api.nvim_feedkeys(keys, "x", false)
+end
+
+return M
