770d7b340dff6b575ed8b6ce4d750968ebe96d4d
johny salas <johmsalas@gmail.com>
2023-11-23 13:36:47 -0500

    LSP rename integration tests setup

---
+++ tests/textcase/lsp/fixtures/component-snake-case.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const component = (props: {}) => {
+  const variable_to_be_tested = 'hello world'
+  return (
+    <div>
+      {variable_to_be_tested}
+    </div>
+  )
+}
--- tests/textcase/lsp/lsp_spec.lua
+++ tests/textcase/lsp/lsp_spec.lua
@@ -1,1 +1,53 @@
-describe("LSP", function() end)
+local test_helpers = require("tests.test_helpers")
+local textcase = require("textcase")
+
+local cur_dir = vim.fn.expand("%:p:h")
+
+---@param path string
+function M.read_file(path)
+  local fd = vim.loop.fs_open(path, "r", 438)
+  local fstat = vim.loop.fs_fstat(fd)
+  local contents = vim.loop.fs_read(fd, fstat.size, 0)
+  vim.loop.fs_close(fd)
+  return contents
+end
+
+describe("LSP", function()
+  describe("Rename", function()
+    before_each(function()
+      textcase.setup({})
+    end)
+
+    it("Should be triggered on keybinding", function()
+      local path = cur_dir .. "/tests/textcase/lsp/fixtures/component-camel-case.tsx"
+      local cmd = " silent exe 'e " .. path .. "'"
+      vim.cmd(cmd)
+      vim.bo.filetype = "typescriptreact"
+      -- allow tsserver start
+
+      local max_seconds = 30
+      local curr_seconds = 0
+      local ts_server_started = false
+      while curr_seconds < max_seconds and not ts_server_started do
+        curr_seconds = curr_seconds + 0.5
+        vim.wait(500, function() end)
+        vim.cmd("LspInfo")
+        ts_server_started = vim.fn.search("1 client(s) attached to this buffer")
+        test_helpers.execute_keys("q")
+      end
+
+      assert.is.truthy(ts_server_started)
+      -- remove
+      vim.wait(200, function() end)
+      test_helpers.execute_keys("/variableToBeTested<CR>gaS")
+      --
+      -- -- allow tsserver to rename the variable
+      vim.wait(2000, function() end)
+      local content = test_helpers.get_buf_lines()
+
+      local expected_code = M.read_file("./tests/textcase/lsp/fixtures/component-snake-case.tsx")
+      assert.are.same(table.concat(content, "\n"), expected_code)
+      vim.wait(2000, function() end)
+    end)
+  end)
+end)
