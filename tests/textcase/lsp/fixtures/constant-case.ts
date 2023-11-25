5a28ba832812e09e5182c145b7f80dfb47078e11
Peter Morgenstern <mail@petergundel.de>
2023-11-25 17:50:32 +0100

    Chore: Add second test case to LSP spec

---
+++ tests/textcase/lsp/fixtures/constant-case.ts
@@ -0,0 +1,4 @@
+export const doSomething = () => {
+  const VARIABLE_TO_BE_TESTED = 'hello world'
+  return VARIABLE_TO_BE_TESTED
+}
--- tests/textcase/lsp/lsp_spec.lua
+++ tests/textcase/lsp/lsp_spec.lua
@@ -23,16 +23,21 @@ describe("LSP", function()
   describe("Rename", function()
     before_each(function()
       textcase.setup({})
-    end)
 
-    it("Should be triggered on keybinding", function()
       local path = "./tests/textcase/lsp/fixtures/camel-case.ts"
       local cmd = " silent exe 'e " .. path .. "'"
       vim.cmd(cmd)
       vim.bo.filetype = "typescript"
 
       wait_for_language_server_to_start()
+    end)
 
+    after_each(function()
+      -- Close the buffer so the next test can open it again.
+      vim.cmd("silent exe 'bd! %'")
+    end)
+
+    it("Should be triggered on keybinding for snake case", function()
       test_helpers.execute_keys("/variableToBeTested<CR>gaS")
       local content = nil
       test_helpers.wait_for(5 * 1000, function()
@@ -42,6 +47,19 @@ describe("LSP", function()
       end)
 
       local expected_code = test_helpers.read_file("./tests/textcase/lsp/fixtures/snake-case.ts")
+      assert.are.same(table.concat(content, "\n"), expected_code)
+    end)
+
+    it("Should be triggered on keybinding for constant case", function()
+      test_helpers.execute_keys("/variableToBeTested<CR>gaN")
+      local content = nil
+      test_helpers.wait_for(5 * 1000, function()
+        content = test_helpers.get_buf_lines()
+        local found_modified_variable = not not string.find(content[2], "VARIABLE_TO_BE_TESTED")
+        return found_modified_variable
+      end)
+
+      local expected_code = test_helpers.read_file("./tests/textcase/lsp/fixtures/constant-case.ts")
       assert.are.same(table.concat(content, "\n"), expected_code)
     end)
   end)
