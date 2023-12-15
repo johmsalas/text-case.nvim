353962265a1051235f2ca5b1974643ad9ce1f502
Peter Morgenstern <mail@petergundel.de>
2023-12-15 07:18:31 +0100

    test(Subs): Add test case for whole buffer

--- tests/textcase/plugin/subs_spec.lua
+++ tests/textcase/plugin/subs_spec.lua
@@ -122,6 +122,20 @@ describe("plugin", function()
             "lorem-ipsum nunc-dolor-sit amet",
           },
         },
+        -- Test case for whole buffer
+        {
+          keys = "<CMD>Subs/lorem/nunc<CR>",
+          buffer_lines = {
+            "LoremIpsum LoremDolorSit amet",
+            "LOREM_IPSUM lorem_dolor_sit amet",
+            "lorem-ipsum lorem-dolor-sit amet",
+          },
+          expected = {
+            "NuncIpsum NuncDolorSit amet",
+            "NUNC_IPSUM nunc_dolor_sit amet",
+            "nunc-ipsum nunc-dolor-sit amet",
+          },
+        },
       },
     }
 
