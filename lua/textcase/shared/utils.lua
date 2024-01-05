b812d890220bf9df358df3a5683b171b46d29c75
Johnny M. Salas <johmsalas@gmail.com>
2024-01-05 07:36:40 -0500

    fix(subs): Avoid modifying a match that has already been altered. (#147)
    * test(subs): Added test when Subs replaces a string and the replacement ends with the replaced string
    
    * fix(subs): Avoid modifying already altered matches

--- lua/textcase/shared/utils.lua
+++ lua/textcase/shared/utils.lua
@@ -270,9 +270,17 @@ function utils.get_current_word_info()
 end
 
 function utils.get_list(str, mode)
+  -- Assuming forward lookup, if Foo is modified to BarFoo, the cursor will remain in Bar,
+  -- using searchpos, the next occurrence of Foo will be the second part of BarFoo which was
+  -- already modified, entering an infinite loop, that will result into: BarBarBarBar...BarFoo
+  --
+  -- for that reason, search should be executed backwards to avoid including an edited match.
+
+  -- TODO: Optimize replacement to run only in a selected region, currently it is running in the whole buffer
+  local search_options = "b"
   local limit = 0
   local initial = nil
-  local next = vim.fn.searchpos(str)
+  local next = vim.fn.searchpos(str, search_options)
 
   local region = utils.get_visual_region(nil, true, mode)
 
@@ -283,7 +291,7 @@ function utils.get_list(str, mode)
         initial = { next[1], next[2] }
       end
     end
-    next = vim.fn.searchpos(str)
+    next = vim.fn.searchpos(str, search_options)
 
     if initial == nil then
       initial = false
@@ -294,7 +302,7 @@ function utils.get_list(str, mode)
 
   return function()
     limit = limit - 1
-    next = vim.fn.searchpos(str)
+    next = vim.fn.searchpos(str, search_options)
 
     if utils.is_empty_position(next) then
       return nil
@@ -310,7 +318,7 @@ function utils.get_list(str, mode)
 
     while not utils.is_cursor_in_range(next, region) do
       limit = limit - 1
-      next = vim.fn.searchpos(str)
+      next = vim.fn.searchpos(str, search_options)
       if utils.is_empty_position(next) then
         return nil
       end
