d6122bcf12ebca7751af4a2e082dec6ed5e95386
John Hui <11800204+j-hui@users.noreply.github.com>
2024-07-13 17:07:30 -0400

    fix(healthcheck): use whichkey.add instead of whichkey.register

--- lua/textcase/extensions/whichkey.lua
+++ lua/textcase/extensions/whichkey.lua
@@ -1,26 +1,29 @@
 local M = {}
 
-function M.register(mode, mappings)
-  local opts = {
-    n = {
-      mode = "n",
-      buffer = nil,
-      silent = true,
-      noremap = true,
-      nowait = true,
-    },
-    x = {
-      mode = "x",
-      buffer = nil,
-      silent = true,
-      noremap = true,
-      nowait = true,
-    },
-  }
-
+function M.register_prefix(mode, prefix, name)
   local ok, whichkey = pcall(require, "which-key")
   if ok then
-    whichkey.register(mappings, opts[mode])
+    if whichkey.add then
+      -- whichkey.register() is deprecated in favor of whichkey.add()
+      whichkey.add({
+        prefix,
+        group = name,
+        mode = mode,
+        buffer = nil,
+        silent = true,
+        noremap = true,
+        nowait = true,
+      })
+    else
+      -- fallback to whichkey.register() if whichkey.add() is unavailable
+      whichkey.register({ [prefix] = { name = name } }, {
+        mode = mode,
+        buffer = nil,
+        silent = true,
+        noremap = true,
+        nowait = true,
+      })
+    end
   end
 end
 
